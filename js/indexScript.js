4824422c3dcaf221c17be179851e73d7d2c580c7
Alejandro <60447710+aaleexLpz@users.noreply.github.com>
2025-03-28 00:47:48 +0100

    Media queries de index finalizadas

--- js/indexScript.js
+++ js/indexScript.js
@@ -1,3 +1,11 @@
 document.getElementById("contenedorMasInformacion").addEventListener("click", function() {
     document.getElementById("divSeparador").scrollIntoView({ behavior: "smooth" });
 });
+
+const menuToggle = document.getElementById('menuToggle');
+const navBarMovil = document.getElementById('NavBarMovil');
+
+menuToggle.addEventListener('click', function() {
+    event.preventDefault();
+    navBarMovil.classList.toggle('show');
+});
